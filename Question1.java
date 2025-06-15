f27da746fdec4e9cf1b68ed1d354b7ce7d4ee57e
Disha rajpoot <rajpootdisha42@gmail.com>
2025-06-15 16:01:30 +0530

    changes

--- Question1.java
+++ Question1.java
@@ -1,21 +1,20 @@
 public class Question1 {
-    public static void main(String args[]){
-        int[] arr = {23,4,12,78,56,43,98,1};
+     public static void main(String args[]){
+         int[] arr = {23,4,12,78,56,43,98,1};
 
         int min = arr[0];
-        int max = arr[0];
+         int max = arr[0];
 
-        for(int i = 1; i<arr.length;i++){
-            if(arr[i] < min) {
-                min = arr[i];
-            }
-            if(arr [i]> max){
-                max = arr[i];
+         for(int i = 1; i<arr.length;i++){
+             if(arr[i] < min) {
+               min = arr[i];
+             }
+             if(arr [i]> max){
+                 max = arr[i];
             }
         }
-        System.out.println("Mininum element:" + min);
-        System.out.println("Maximum element:" + max);
+         System.out.println("Mininum element:" + min);
+       System.out.println("Maximum element:" + max);
 
-    }
-}
+     }
 
