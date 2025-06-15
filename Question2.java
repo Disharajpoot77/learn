093df51bf7b2760bc41067157c6fac824cc70a4e
Disha rajpoot <rajpootdisha42@gmail.com>
2025-06-15 12:44:07 +0530

    change

---
+++ Question2.java
@@ -0,0 +1,18 @@
+public class Question2 {
+	    public static void main(String args[]){
+         int[] arr = {1,2,3,4,5};
+         int[] reversed = new int[arr.length];
+
+         for (int i = 0; i < arr.length; i++){
+            reversed[i] = arr[arr.length-1-i];
+         }
+
+         System.out.println("Reversed array" );
+         for(int i = 0; i< reversed.length;i++){
+            System.out.print(reversed[i] + " ");
+         }
+
+        
+
+    }
+}
